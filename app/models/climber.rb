b1aa5138c0ac2a7654c5241afa353b1c0d2842fa
github username <github email address>
2019-10-01 17:58:44 -0700

    built climber methods

--- app/models/climber.rb
+++ app/models/climber.rb
@@ -2,6 +2,26 @@ class Climber < ActiveRecord::Base
     has_many :climbs
     has_many :routes, through: :climbs
 
-    
+
+    def list_most_popular_route
+
+    end
+
+    def retrieve_climbs
+        Climb.all.select { |info| info.climber_id == self.id }
+    end
+
+    def delete_climb                     ############## coming back to this l8er sk8er!! ###################
+        mine = retrieve_climbs
+        puts mine
+    end                                    ####################  end coming back  ################
+
+    def climbs_by_location(location)
+        Route.all.select { |info| info.location == location }
+    end
+
+    def climbs_by_my_location
+        Route.all.select { |info| info.location == self.location }
+    end
 
 end
